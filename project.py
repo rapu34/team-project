37b2b1537d462144fe22f314a8ab121f0f292794
cswcjt <jungtaek0227@gmail.com>
2022-09-23 21:19:09 +0900

    feat: Monty Hall
    fun practice

--- project.py
+++ project.py
@@ -1,2 +1,32 @@
-print("Let's Begin")
+import random
 
+def play_monty_hall(choice):
+    prize = ['goat', 'car', 'goat']
+
+    random.shuffle(prizes)
+
+    while True:
+        opening_door = random.randrange(len(prizes))
+        if prizes[opening_door] != 'car' and choice-1 != opening_door:
+            break
+
+    opening_door = opening_door + 1
+    print('We are opening the door number-%d' % (opening_door))
+
+    options = [1,2,3]
+    options.remove(choice)
+    options.remove(opening_door)
+    switching_door = option[0]
+
+    print('Now, do you want to swich to door number-%d? (yes/no)' %(switching_door))
+    answer = input()
+    if answer == 'yes':
+        result = switching_door - 1
+    else:
+        result = choice - 1
+
+    print('And your prize is ....', prizes[result].upper())
+
+choice = int(input('Which door do you want to choose? (1,2,3): '))
+
+play_monty_hall(choice)
